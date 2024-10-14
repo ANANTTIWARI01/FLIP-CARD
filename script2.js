1182d1492d627fd023b458c4a5a20588b7cb0f74
ANANTTIWARI01 <bhaukalbaba45@gmail.com>
2024-10-14 15:52:06 +0530

    first commit

--- script2.js
+++ script2.js
@@ -1,48 +1,82 @@
+let imagearr = ["img1.webp", "img2.png", "img3.jpg", "img4.webp", "img5.webp", "img6.jpg"]
+let Parent = document.querySelector("#parent")
+let startbutton = document.querySelector("button")
+let flipcardback = document.querySelectorAll(".flip-card-back")
+let frontdivimage = document.querySelectorAll(".flip-card-front img")
+let timerDiv = document.querySelector("p")
+let blankarr = []
+let openimage = 0
+let openstoreimage = []
+let count = 0
+let timmer = 0
 
-startBtn.addEventListener('click',function(){
-    startBtn.style.display="none"
-    Parent.style.display="block"
+let mainimage = [...imagearr, ...imagearr]
+
+
+
+startbutton.addEventListener('click', function () {
+    startbutton.style.display = "none"
+    Parent.style.display = "block"
+    sendimage()
+    settimer()
 })
 
-flipcard.forEach(flip_card => {
-    
-flip_card.addEventListener("click",function(){
-    ++count
-    console.log(count);
-    sendingimage()
-    
-  
+function sendimage() {
+    for (let i = 0; i < mainimage.length; i++) {
+        let image = document.createElement("img")
+        image.src = mainimage[getrandomImage()]
+        flipcardback[i].append(image)
+    }
+}
+
+
+frontdivimage.forEach((frontimage) => {
+    frontimage.addEventListener('click', function () {
+        openimage++
+        frontimage.parentElement.parentElement.classList.add('backside')
+        openstoreimage.push(frontimage.parentElement.nextElementSibling.children[0])
+        console.log(openstoreimage);
+        setTimeout(() => {
+            if (openimage > 1) {
+                if (openstoreimage[0].src === openstoreimage[1].src) {
+                    openimage = 0
+                    openstoreimage.length = 0
+                    count++
+                }
+                else {
+                    openstoreimage.forEach((image) => {
+                        image.parentElement.parentElement.classList.remove('backside')
+                    })
+                    openimage = 0
+                    openstoreimage.length = 0
+                }
+            }
+        }, 500)
+    })
 })
 
-});
 
 
-function randomvalue(){
-    return Math.floor(Math.random()*arr.length)
-}
+function getrandomImage() {
+    let randomvalue = Math.floor(Math.random() * mainimage.length)
+    if (blankarr.includes(randomvalue)) return getrandomImage()
 
-
-function sendingimage(){
-
-    for(let i=0;i<arr.length*2;i++){    
-    let random= randomvalue()
-    let Randomimage = arr[random]
-
-    let image= document.createElement("img")
-    image.src=Randomimage
-let Flipcardback = document.querySelector(".flip-card-back")
-     
-    
-    Flipcardback[i].append(image)
-    e.target.style.transform="rotateY(180deg)"
+    else {
+        blankarr.push(randomvalue)
+        return randomvalue
+    }
 
 }
-}
 
-
-function checkingsimilarimg(value){
-//   const here  =value
-  data.push(value)
-  console.log(data);
-  
-}
+    function settimer(){
+        let set = setInterval(() => {
+            timerDiv.innerHTML=timmer=timmer+1
+                if (timmer === 60 || count==6) {
+                
+                clearInterval(set)
+            }
+            // screenthird()
+            
+        }, 1000);
+        }
+        
